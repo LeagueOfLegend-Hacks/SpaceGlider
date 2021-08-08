807058faa35a7fe09717f12373f6779b7d419db4
Dencelle <dencelle@gmail.com>
2021-08-08 14:03:48 -0500

    pytools setup

---
+++ pytools/README.md
@@ -0,0 +1,1 @@
+# SpaceGlider - pytools
---
+++ pytools/pytools.py
@@ -0,0 +1,90 @@
+from idc import *
+from idautils import *
+
+def DecToHex(Addr):
+	return "0%0.2X" % Addr
+
+def MakeEnum(enumName, offsetArray):
+    print ("enum class %s\\r\\n{" % enumName)
+    for offset in offsetArray:
+        if len(offset[0]) == 0:
+            print ("")
+            continue
+        if type(offset[1]) is str:
+            print ("   %s = %s," % ( offset[0], offset[1]))
+            continue
+
+        fncValue = offset[1] if offset[1] != -1 else 0x0
+
+        locByName = idc.get_name_ea_simple(offset[0])
+        isMismatch = locByName == 0x0
+
+        if locByName == BADADDR:
+            locByName = fncValue
+        
+        if locByName > idaapi.get_imagebase():
+            set_name(locByName, offset[0])
+
+        print ("   %s = %s,%s" % (offset[0], hex(locByName-idaapi.get_imagebase()), '// Possible mismatch' if isMismatch else ''))
+
+    print ("};\\r\\n")
+    
+def FindFunctionAddr(name, offset, operandValue):
+    address = idc.find_binary(0, SEARCH_DOWN, "\"" + name + "\"")
+    dword = -1
+    
+    if address == BADADDR:
+        return BADADDR
+    
+    xrefs = XrefsTo(address)
+    for xref in xrefs:
+        dword = xref.frm + offset
+    
+    if dword == BADADDR:
+        return BADADDR
+    
+    return idc.get_operand_value(dword, operandValue)
+
+def FindOffsetPattern(Pattern, Operand):
+	addr = idc.find_binary(0, SEARCH_DOWN, Pattern)
+	if addr == BADADDR: return 0
+	
+	return idc.get_operand_value(addr, Operand)
+
+def FindFunctionFirstXRef(name):
+    address = idc.find_binary(0, SEARCH_DOWN, "\"" + name + "\"")
+    dword = BADADDR
+    
+    if address == BADADDR:
+        return BADADDR
+    
+    xrefs = XrefsTo(address)
+    for xref in xrefs:
+        dword = xref.frm
+    
+    try:
+        return idaapi.get_func(dword).start_ea
+    except Exception:
+        return -1
+
+def FindFunctionByPatternStartEA(pattern):
+    address = idc.find_binary(0, SEARCH_DOWN, pattern)
+    if address == BADADDR:
+        return BADADDR
+    
+
+    try:
+        return idaapi.get_func(address).start_ea
+    except Exception:
+        return -1
+        
+def FindFuncCall(Pattern):
+    addr = idc.find_binary(0, SEARCH_DOWN, Pattern)
+    if addr == BADADDR: return 0
+    return idc.get_operand_value(addr, 0)
+
+def main():
+    MakeEnum("Functions", [
+        ["SetPlayerStats", FindFunctionFirstXRef("mPhysicalDamagePercentageModifier")],
+    ])
+main()
