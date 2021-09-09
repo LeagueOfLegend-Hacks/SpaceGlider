1e079f26f8e7ea7f36e98013385a3722f7acaf3d
Dencelle <dencelle@gmail.com>
2021-09-09 03:27:54 -0700

    11.18 (mostly) updated

--- internal/Patchables/Offsets.h
+++ internal/Patchables/Offsets.h
@@ -17,42 +17,37 @@ namespace Offsets {
 		kInvalid = 0x4
 	};
 	enum class Functions {
-		GetPing = 0x320610,
-		OnProcessSpell = 0x4f8840,
-		OnNewPath = 0x27e4c0,
-		WorldToScreen = 0x971F30,
-		GetFirstObject = 0x278690,
-		GetNextObject = 0x279090,
-		IsAlive = 0x194180,
-		IsHero = 0x1A7080,
-		IsMissile = 0x1a70e0,
-		IsMinion = 0x1a70c0,
-		IsInhib = 0x1a6f00,
-		IsBaron = 0x165B20,
-		IsNexus = 0x1a7000,
-		IsTurret = 0x1a6ec0,
-		GetAttackDelay = 0x276090,
-		GetAttackCastDelay = 0x275f90,
-		OnCreateObject = 0x27E1E0,	//0xAE
-		OnDeleteObject = 0x276BF0,	//0x151
-		OnFinishCast = 0x4EAB00,
-		IssueClick = 0x5AE550,
-		IssueSpell = 0x5DCE20
+		GetPing = 0x322500,								// 11.18		55 8B EC 83 EC 08 0F B6
+		OnProcessSpell = 0x51D350,						// 11.18		E8 ?? ?? ?? ?? 8B CE E8 ?? ?? ?? ?? 80 BE ?? ?? ?? ?? ?? D8
+		WorldToScreen = 0x972BA0,						// 11.18		83 EC 10 56 E8 ? ? ? ? 8B 08
+		GetFirstObject = 0x27A880,						// 11.18		8B 44 24 04 56 8B 71 18
+		GetNextObject = 0x27B280,						// 11.18		E8 ? ? ? ? 8B F0 85 F6 74 21 0F 1F 44 00 ?
+		IsAlive = 0x13EDF0,								// 11.18		56 8B F1 8B 06 8B 80 ? ? ? ? FF D0 84 C0 74 19
+		IsHero = 0x1A94C0,								// 11.18		E8 ? ? ? ? 83 C4 04 84 C0 74 52
+		IsMissile = 0x1A9520,							// 11.18		E8 ? ? ? ? 83 C4 04 84 C0 74 3F
+		IsMinion = 0x1a70c0,							// 11.17		E8 ? ? ? ? 83 C4 04 80 7F 26 06
+		IsInhib = 0x1a6f00,								// 11.17		E8 ? ? ? ? 55 88 44 24 1F
+		IsBaron = 0x165B20,								// 11.17		56 81 C1 ? ? ? ? E8 ? ? ? ? 68
+		IsNexus = 0x1a7000,								// 11.17		E8 ? ? ? ? 55 88 44 24 20
+		IsTurret = 0x1a6ec0,							// 11.17		E8 ? ? ? ? 83 C4 04 84 C0 75 ?
+		GetAttackDelay = 0x2781A0,						// 11.18		8B 44 24 04 51 F3
+		GetAttackCastDelay = 0x2780a0,					// 11.18		83 EC 0C 53 8B 5C 24 14 8B CB 56
+		IssueClick = 0x5F5B40,							// 11.18		E8 ? ? ? ? 84 C0 74 12 5F
 	};
 	enum class Data {
-		NetClient = 0x30E223C,
-		LocalPlayer = 0x30D2FD4,						// 11.18
-		GameTime = 0x30CB3AC,							// 11.18
-		ObjectManager = 0x18363C0,						// 11.18
-		ManagerTemplate_Minions = 0x30CD588,
-		ManagerTemplate_AttackableUnits = 0x30CD584,
-		ManagerTemplate_Turrets = 0x30D1AF8,
-		ManagerTemplate_Shops = 0x30CE500,
-		ManagerTemplate_Inhibitors = 0x30DB7F8,
-		MangerTemplate_Buildings = 0x183B9DC,
-		ManagerTemplate_Heros = 0x183C1A4,
-		HudInstance = 0x183e1dc,
-		D3DRender = 0x30FA8D4							// 11.18
+		NetClient = 0x30dab58,							// 11.18		8B 0D ? ? ? ? 85 C9 74 07 8B 01 6A 01 FF 50 08 8B
+		LocalPlayer = 0x30D2FD4,						// 11.18		A1 ?? ?? ?? ?? 85 C0 74 07 05 ?? ?? ?? ?? EB 02 33 C0 56
+		GameTime = 0x30CB3AC,							// 11.18		F3 0F 11 05 ? ? ? ? 8B 49
+		ObjectManager = 0x18363C0,						// 11.18		8B 0D ? ? ? ? 89 74 24 14
+		ManagerTemplate_Minions = 0x30CD588,			// 11.17		A1 ?? ?? ?? ?? 53 55 8B 6C 24 1C
+		ManagerTemplate_AttackableUnits = 0x30CD584,	// 11.17
+		ManagerTemplate_Turrets = 0x30D1AF8,			// 11.17		A1 ? ? ? ? 53 56 8B 70 04
+		ManagerTemplate_Shops = 0x30CE500,				// 11.17
+		ManagerTemplate_Inhibitors = 0x30DB7F8,			// 11.17
+		MangerTemplate_Buildings = 0x183B9DC,			// 11.17
+		ManagerTemplate_Heros = 0x1833D88,				// 11.18		8B 0D ?? ?? ?? ?? 50 8D 44 24 18
+		HudInstance = 0x18363F8,						// 11.18		8B 0D ? ? ? ? 6A 00 8B 49 34 E8 ? ? ? ? B0 01 C2
+		D3DRender = 0x30FA8D4							// 11.18		8B 15 ? ? ? ? 83 EC 08
 	};
 	enum class GameObject {
 		Index = 0x20,
