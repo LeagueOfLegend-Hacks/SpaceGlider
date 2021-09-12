047090a9a5d5a86c9e4232c4f6acb0b4db89b92d
bckd00r <48347237+bckd00r@users.noreply.github.com>
2021-09-12 05:58:05 +0300

    Some good things :D

--- internal/Plugins/Utilities/WaypointTracker.h
+++ internal/Plugins/Utilities/WaypointTracker.h
@@ -5,16 +5,53 @@
 
 namespace WaypointTracker
 {
+	void drawSpell(GameObject* unit, CSpellBase* spell, Vector3 pos)
+	{
+		ImColor color = spell->ready() ? ImColor(255, 255, 255) : ImColor(120, 120, 120);
+		if (spell->spellInfo()->spellData()->mana(spell->level) <= unit->Mana)
+			color = ImColor(49, 97, 121);
+		render.draw_image(spell->spellInfo()->spellData()->name, pos, Vector2(24, 24), color);
+		if (spell->remainingCooldown() > 0.0f) {
+			Vector2 worldPos = riot_render->WorldToScreen(pos);
+			render.draw_text(Vector2(worldPos.x + 4, worldPos.y + 5), std::to_string(spell->remainingCooldown()).c_str(), false);
+		}
+	}
+
 	void ShowWaypoints()
 	{
-		for each (GameObject * hero in ObjectManager::HeroList())
+		Vector3 pos = ObjectManager::GetLocalPlayer()->Position;
+		pos.x -= 70;
+
+		pos.x += 35;
+		drawSpell(ObjectManager::GetLocalPlayer(), ObjectManager::GetLocalPlayer()->SpellBook.spellId(0), pos);
+		pos.x += 35;
+		drawSpell(ObjectManager::GetLocalPlayer(), ObjectManager::GetLocalPlayer()->SpellBook.spellId(1), pos);
+		pos.x += 35;
+		drawSpell(ObjectManager::GetLocalPlayer(), ObjectManager::GetLocalPlayer()->SpellBook.spellId(2), pos);
+		pos.x += 35;
+		drawSpell(ObjectManager::GetLocalPlayer(), ObjectManager::GetLocalPlayer()->SpellBook.spellId(3), pos);
+		for (GameObject * hero : ObjectManager::HeroList())
 		{
-			if (hero->IsEnemyTo(ObjectManager::GetLocalPlayer()))
-			{
-				if (hero->IsVisible && Functions::IsAlive(hero))
-					render.draw_line3D(hero->Position, hero->GetAIManager()->NavEndPos, ImColor(255, 0, 0, 255), 3.0f);
+			if (!hero->IsVisible || !Functions::IsAlive(hero) || hero->IsAllyTo(ObjectManager::GetLocalPlayer()))
+				continue;
+			Vector3 pos = hero->Position;
+			pos.x -= 70;
+
+			pos.x += 35;
+			drawSpell(hero, hero->SpellBook.spellId(0), pos);
+			pos.x += 35;
+			drawSpell(hero, hero->SpellBook.spellId(1), pos);
+			pos.x += 35;
+			drawSpell(hero, hero->SpellBook.spellId(2), pos);
+			pos.x += 35;
+			drawSpell(hero, hero->SpellBook.spellId(3), pos);
+			if (!hero->GetAIManager()->Moving)
+				continue;
+			if (hero->IsVisible && Functions::IsAlive(hero)) {
+				render.draw_image("singletarget", hero->GetAIManager()->NavEndPos, Vector2(36, 36), ImColor(255, 255, 255));
 			}
 		}
+
 	}
 
 	void OnUnload()
